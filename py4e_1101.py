da68b91b6b21077dbccd6fda2db20df15efe4596
Dave <davemarandi@gmail.com>
2024-04-02 11:48:28 -0400

    changed regex method from findall to search. Still get slightly different number from what chuck wants for java$

--- py4e_1101.py
+++ py4e_1101.py
@@ -1,13 +1,15 @@
-file = open ("/mbox.txt")       #mbox.txt or mbox-short.txt
 import re
-inp=input("Please enter a regular expression \n>>>")
-regular=str(inp)
+file = input ("Enter the file path \n>>>")       #mbox.txt or mbox-short.txt
+regex=input("Please enter a regular expression \n>>>")
+fhand = open (file,'r')
+
+#regular=str(inp)
 counter=0
-print (regular)
-for line in file:
+print (regex)
+for line in fhand:
     line=line.rstrip()
-    for line in file:
-        x=re.findall(regular, line)
-        if len(x)>0:
+    for line in fhand:
+        if re.search(regex,line):
+        #if len(x)>0:
             counter+=1
-print ("Your file had", counter, "lines that matched", inp)
+print ("Your file had", counter, "lines that matched", regex)
