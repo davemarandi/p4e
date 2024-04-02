5c79b9e8ab568ac5d418d7bdc519e6538d1962de
Dave <davemarandi@gmail.com>
2024-04-02 11:56:21 -0400

    fixed regex exercise 1

--- py4e_1101.py
+++ py4e_1101.py
@@ -1,15 +1,12 @@
 import re
+
 file = input ("Enter the file path \n>>>")       #mbox.txt or mbox-short.txt
 regex=input("Please enter a regular expression \n>>>")
-fhand = open (file,'r')
 
-#regular=str(inp)
+fhand = open (file,'r')
 counter=0
-print (regex)
 for line in fhand:
     line=line.rstrip()
-    for line in fhand:
-        if re.search(regex,line):
-        #if len(x)>0:
-            counter+=1
+    if re.search(regex,line):
+        counter+=1
 print ("Your file had", counter, "lines that matched", regex)
